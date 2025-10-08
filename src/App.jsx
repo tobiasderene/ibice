1ae0e35bdfa7487ac6c18e349bc19728b0432036
tobiasderene <tobiasderene@gmail.com>
2025-10-08 16:42:35 -0300

    no aboutus in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Products from './components/Products';
-import AboutUs from './components/AboutUs';
 
 function App() {
   return (
